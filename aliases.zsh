2494506a4e40c2546e7d53e8ba4e271d6f55b7b3
Anna Mockenhaupt <75340293+madeyemocki@users.noreply.github.com>
2023-03-03 10:11:34 +0100

    aliases and gitconfig

---
+++ aliases.zsh
@@ -0,0 +1,15 @@
+####### ZON ##########
+# batou
+alias batou='./batou deploy local'
+
+# zeit.web & friedbert
+alias zon='cd ~/work/friedbert-deployment/work/source/zeit.web'
+alias zonroot='cd ~/work/friedbert-deployment'
+alias zonserve='zonroot && bin/serve --reload'
+alias zonserve-staging='zonroot && bin/serve staging --reload'
+alias zonserve-production='zonroot && bin/serve production --reload'
+alias zontest='zonroot && ./bin/test'
+
+#show this file
+alias helpalias='less ~/.aliases.zsh'
+
